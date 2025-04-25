7f25ec8d837a6e1b6e0ee68b4b478f7e595bd7ce
Aris Jirat <arisjiratkurniawan@gmail.com>
2025-04-26 03:17:47 +0700

    init

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,26 @@
 import { Elysia } from "elysia";
+import mysql from "mysql2/promise";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const {
+  MYSQL_USER,
+  MYSQL_PASSWORD,
+  MYSQL_DATABASE,
+  MYSQL_HOST = "db",
+} = process.env;
 
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+const app = new Elysia();
+
+const pool = mysql.createPool({
+  host: MYSQL_HOST,
+  user: MYSQL_USER,
+  password: MYSQL_PASSWORD,
+  database: MYSQL_DATABASE,
+});
+
+app.get("/users", async () => {
+  const [rows] = await pool.query("SELECT * FROM users");
+  return rows;
+});
+
+app.listen(3000);
+console.log("🦊 Elysia running at http://localhost:3000");
